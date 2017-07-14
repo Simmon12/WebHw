33b52c3b5bc68104307686e15b7ec80c2639eeb2
litchi <314749816@qq.com>
2017-07-14 21:58:12 +0800

    迷宫与地鼠

---
+++ 迷宫与地鼠/maze.js
@@ -0,0 +1,84 @@
+var gameStart = false;
+    hasCheat = false;
+    gameOver = false;
+
+window.onload = function () {
+    var start = document.getElementById('start');
+        end   = document.getElementById('end');
+        blocks = document.getElementsByClassName('block');
+        cheatCheck = document.getElementById('cheat-check');
+        maze = document.getElementById('maze');
+    start.onmouseover = startGame;
+    end.onmouseover = endGame;
+    maze.onmouseleave = outMaze;
+
+    for(var i = 0; i < blocks.length; i++) {
+        blocks[i].onmouseover = overBlock;
+    }
+
+    // dont select anything when double click div/span/p
+    document.onselectstart = function () {
+        return false;
+    }
+}
+
+function startGame() {
+    display("");
+    // change color
+    var blocks = document.getElementsByClassName('block block-changed');
+    for(var i = 0; i < blocks.length; i++) {
+        blocks[i].className = blocks[i].className.replace(' block-changed','');
+    }
+    gameStart = true;
+    hasCheat  = false;
+    gameOver  = false;
+}
+
+function endGame() {
+    if(!gameOver && (!gameStart || hasCheat)) {
+        display("Don't cheat, you should start from the 'S' and move " + "to the 'E' inside the maze!");
+    } else if(gameStart && !gameOver) {
+        display("You win");
+    }
+    gameStart = false;
+    hasCheat = false;
+}
+
+function overBlock(event) {
+    if(!gameStart || gameOver) return;
+    gameOver = true;
+    var className = event.target.className;
+    if(className.indexOf(' block-changed') == -1) {
+        event.target.className += ' block-changed';
+    }
+    display("You lose");
+}
+
+// function outBlock(event) {
+//     var className = event.target.className;
+//     if(className.indexOf('block-changed') != -1) {
+//         event.target.className = className.replace(' block-changed', '');
+//     }
+// }
+
+function outMaze() {
+    hasCheat = true;
+    var blocks = document.getElementsByClassName('block block-changed');
+    for(var i = 0; i < blocks.length; i++) {
+        blocks[i].className = 'block';
+    }
+}
+
+// function cheat() {
+//     hasCheat = true;
+// }
+
+function display(info) {
+    var notice = document.getElementById('notice');
+    if(info == "") {
+        notice.className = 'invisible';
+    }else {
+        notice.className = '';
+        notice.textContent = info;
+    }
+}
---
+++ 迷宫与地鼠/mole.js
@@ -0,0 +1,85 @@
+var currentMole = -1;
+    currentTime = 30;
+    score       = 0;
+    timeIntervalId = null;
+
+window.onload = function () {
+    // genrate radio-type inputs
+    var inputFrag = document.createDocumentFragment();
+    for(var i = 0; i < 60; i++) {
+        var newInput = document.createElement('input');
+        newInput.name = 'mole';
+        newInput.type = 'radio';
+        newInput.onclick = hitMole;
+        inputFrag.appendChild(newInput);
+    }
+    document.getElementById('whac-a-mole').appendChild(inputFrag);
+    document.getElementById('sta-end').addEventListener('click', startGame);
+
+    // dont select anything when double click div/span/p
+    document.onselectstart = function () {
+        return false;
+    }
+}
+
+function hitMole(event) {
+    if(currentMole == -1) {
+        currentMole = Math.floor(Math.random()*59);
+    }else if(timeIntervalId != null) {
+        if(document.getElementsByName('mole')[currentMole].checked) {
+            ++score;
+            this.checked = false;
+
+            // dont appear in the same place
+            var randomMole = Math.floor(Math.random() * 59);
+            while(randomMole == currentMole) {
+                randomMole = Math.floor(Math.random() * 59);
+            }
+            currentMole = randomMole;
+        }else {
+            this.checked = false;
+            if(score > 0)
+                --score;
+        }
+    }
+    document.getElementsByName('mole')[currentMole].checked = true;
+    showGameInfo('score', score);
+}
+
+function ticktock() {
+    --currentTime;
+    if(currentTime <= 0) {
+        stopGame();
+    }else {
+        document.getElementsByName('time')[0].value = currentTime.toString();
+    }
+}
+
+function startGame() {
+    // initialize
+    currentMole = -1;
+    currentTime = 30;
+    score = 0;
+    timeIntervalId = setInterval(ticktock, 1000);
+    hitMole();
+
+    showGameInfo('game-status', 'playing');
+    var staEnd = document.getElementById('sta-end');
+    staEnd.removeEventListener('click', startGame);
+    staEnd.addEventListener('click', stopGame);
+}
+
+function stopGame() {
+    clearInterval(timeIntervalId);
+    timeIntervalId = null;
+    showGameInfo('game-status', 'Game Over');
+    showGameInfo('time', 0);
+    alert('Game Over.\n' + 'Your score is: ' + score);
+    var staEnd = document.getElementById('sta-end');
+    staEnd.removeEventListener('click', stopGame);
+    staEnd.addEventListener('click', startGame);
+}
+
+function showGameInfo(info, status) {
+    document.getElementsByName(info)[0].value = status;
+}
